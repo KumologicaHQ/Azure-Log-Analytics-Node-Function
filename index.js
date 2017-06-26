eb4e1a7b4c981606445b053eb4150f624610b871
Graham Hunter <graham@sportsmgmt.net>
2017-06-26 08:27:11 -0700

    Working version

--- index.js
+++ index.js
@@ -3,78 +3,50 @@ module.exports = function (context, req) {
     context.log('Azure Log Analysis Data Collector Function received a request');
 
     // required node.js libraries
-    var https = require('https');
+    var request = require('request');
     var crypto = require('crypto');
 
     // Azure Log Analysis credentials
-    var workspaceId = 'xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx';
-    var sharedKey = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+    // var workspaceId = 'xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx';
+    // var sharedKey = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+
+    var workspaceId = req.body.credentials.workspaceId;
+    var sharedKey = req.body.credentials.sharedKey;
 
     var apiVersion = '2016-04-01';
     var processingDate = new Date().toUTCString();
 
-    // *****************************************************
-    // step one - build authorization signature
+    var body = JSON.stringify(req.body.logEntry);
 
-    var contentLength = Buffer.byteLength(req.body['log-entry'], 'utf8');
+    context.log('Body is ' + body);
 
-    var authorization = 'POST\n' + contentLength + '\napplication/json\nx-ms-date:' + processingDate + '\n/api/logs';
+    var contentLength = Buffer.byteLength(body, 'utf8');
 
-    // encode string using Base64(HMAC-SHA256(UTF8(StringToSign)))
-    authorization = crypto.createHmac('sha256', new Buffer(sharedKey, 'base64')).update(authorization, 'utf-8').digest('base64');
-    authorization = 'SharedKey ' + workspaceId + ':' + authorization;
-    
-    // *****************************************************
-    // step two - build request headers
+    var stringToSign = 'POST\n' + contentLength + '\napplication/json\nx-ms-date:' + processingDate + '\n/api/logs';
+    var signature = crypto.createHmac('sha256', new Buffer(sharedKey, 'base64')).update(stringToSign, 'utf-8').digest('base64');
+    var authorization = 'SharedKey ' + workspaceId + ':' + signature;
 
     var headers = {
         "content-type": "application/json", 
-        "Accept": "application/json",
-        "x-ms-date": processingDate,
-        "Authorization": authorization
+        "Authorization": authorization,
+        "Log-Type": req.body.type,
+        "x-ms-date": processingDate
     };
 
-    // add Log-Type and (optionally) Time-Generated
-    if( req.body.type != undefined ) {
-        headers['Log-Type'] = req.body.type;
-    } else {
-        headers['Log-Type'] = 'No Type';
-    }
+    context.log( 'Request Headers: ' + JSON.stringify(headers) );
 
-    if( req.body.time != undefined ) {
-        headers['time-generated-field'] = req.body['time-generated'];
-    }
+    var url = 'https://' + workspaceId + '.ods.opinsights.azure.com/api/logs?api-version=' + apiVersion;
 
-    // *****************************************************
-    // step three - send request
+    request.post({url: url, headers: headers, body: body}, function (error, response, body) {
+        
+        context.log('error:', error); 
+        context.log('statusCode:', response && response.statusCode); 
+        context.log('body:', body);
 
-    var options = {
-        "hostname": workspaceId + '.ods.opinsights.azure.com',
-        "path": '/api/logs?api-version=' + apiVersion,
-        "port": 443,
-        "method": 'POST',
-        "headers": headers,
-        "body": req.body['log-entry']
-    };
-
-    context.log('Request Options:\n' + JSON.stringify(options, null, 2));
-
-    var req = https.request(options, (res) => {
-    
-        context.log('Reponse Status Code:', res.statusCode);
-
-        res.on('data', (data) => {
-            context.res = { status: res.statusCode, body: data }
-            context.done();
-        });
-
-    });
-
-    req.on('error', (err) => {
-        context.log('https error: ' + err);
-        context.res = { status: 500, body: data }
+        context.res = { 'status': response.statusCode, 'body': body }
         context.done();
+
     });
-    req.end();    
+
 
 };
